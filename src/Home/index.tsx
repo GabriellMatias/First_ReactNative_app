d1cf8c5318f88c0bbe4574d94e3e4d1a014180b3
GabriellMatias <gabriel.goiasat@gmail.com>
2022-10-29 06:28:28 -0300

    :tada: First commit, updating files

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,25 +1,69 @@
-import { useState } from 'react';
-import { Image, SafeAreaView, ScrollView, TextInput, View } from 'react-native';
+import { useState, useEffect, useRef } from "react";
+import { Image, SafeAreaView, ScrollView, TextInput, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Button } from '../components/Button';
-import { PositionChoice } from '../components/PositionChoice';
+import { Header } from "../components/Header";
+import { Button } from "../components/Button";
+import { PositionChoice } from "../components/PositionChoice";
 
-import { styles } from './styles';
-import { POSITIONS, PositionProps } from '../utils/positions';
+import { styles } from "./styles";
+import { POSITIONS, PositionProps } from "../utils/positions";
+
+import { Camera, CameraType } from "expo-camera";
+import { captureRef } from "react-native-view-shot";
+import * as Sharing from 'expo-sharing'
 
 export function Home() {
-  const [positionSelected, setPositionSelected] = useState<PositionProps>(POSITIONS[0]);
+
+  const [photo, setPhotoURI] = useState<null|string>(null)
+  const [hasCameraPermission, SethasCameraPermision] = useState(false);
+  const [positionSelected, setPositionSelected] = useState<PositionProps>(
+    POSITIONS[0]
+  );
+
+  const screenshotRef = useRef(null)
+  const cameraRef = useRef<Camera>(null)
+
+  async function handleTakePicture(){
+    const photo = await cameraRef.current.takePictureAsync()
+    setPhotoURI(photo.uri)
+  }
+
+  async function handleShareScreenshot(){
+    const screenShot = await captureRef(screenshotRef)
+    await Sharing.shareAsync("file://" + screenShot)
+  }
+
+  useEffect(() => {
+    Camera.requestCameraPermissionsAsync().then((response) =>
+      SethasCameraPermision(response.granted)
+    );
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scroll} showsVerticalScrollIndicator={false}>
-        <View>
+      <ScrollView
+        contentContainerStyle={styles.scroll}
+        showsVerticalScrollIndicator={false}
+      >
+        <View ref={screenshotRef}>
           <Header position={positionSelected} />
 
           <View style={styles.picture}>
 
-            <Image source={{ uri: 'https://github.com/rodrigorgtic.png' }} style={styles.camera} />
+            {hasCameraPermission && !photo ? 
+            <Camera 
+            ref={cameraRef}
+            style={styles.camera}
+            type={CameraType.front}
+            /> : 
+              <Image
+                source={{
+                  uri:photo? photo: "https://images.gutefrage.net/media/fragen/bilder/meine-kamera-auf-windows-10-funktioniert-nicht-was-tun/0_big.jpg?v=1584606917000",
+                }}
+                style={styles.camera}
+                onLoad={handleShareScreenshot}
+              />
+            }
 
             <View style={styles.player}>
               <TextInput
@@ -35,7 +79,7 @@ export function Home() {
           positionSelected={positionSelected}
         />
 
-        <Button title="Compartilhar" />
+        <Button title="Compartilhar" onPress={handleTakePicture} />
       </ScrollView>
     </SafeAreaView>
   );
